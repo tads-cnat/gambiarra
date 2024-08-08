a38dd697196a9695842ee8652ef1b18fb740fedc
leveau10 <leovir957@gmail.com>
2024-08-08 11:12:17 -0300

    feat: listagem basica feita

--- mysite/gambiarra/views.py
+++ mysite/gambiarra/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
+def index(request):
+    
+    return render(request, 'gambiarra/index.html')
+
+def listar_chamados(request):
+    chamados = Chamado.objects.all().prefetch_related('bolsistas')
+    context = {'chamados':chamados}
+
+    return render(request, 'gambiarra/listar-chamados.html', context)
