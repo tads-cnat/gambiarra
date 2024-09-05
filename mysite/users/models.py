67c9559c7661d9d7139020676e1c9917731ebc62
Livia Vitória Da Silva <20231014040029@ifrn.local>
2024-09-05 10:59:46 -0300

    Responsividade da tabela do index da dashboard. #72

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -19,6 +19,10 @@ class User(AbstractUser):
     )
 
     def save(self, *args, **kwargs):
+
+        if self.pk and 'password' in self.__dict__ and not self.password.startswith('pbkdf2_'):
+            self.set_password(self.password)
+            
         super().save(*args, **kwargs)
 
         if self.tipo_usuario == '4': 
