9a46aaeeeedcc5049cbfa4169224d1ea62774374
leveau10 <leovir957@gmail.com>
2024-11-06 21:22:14 -0300

    :wrench: build: add postgres

--- gamb-back/mysite/settings.py
+++ gamb-back/mysite/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  # Accessing PostgreSQL from local
+        'PORT': '5432',
     }
 }
 
