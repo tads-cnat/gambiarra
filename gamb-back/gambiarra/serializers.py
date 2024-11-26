88b0b838464397ae7045608f194a571b3daaa93b
Luiz Gustavo Oliveira Rocha <luizgustavo.orocha1@gmail.com>
2024-11-26 00:34:17 -0300

    :heavy_plus_sign: feat: cria o serializer do acessório #142

--- gamb-back/gambiarra/serializers.py
+++ gamb-back/gambiarra/serializers.py
@@ -1,7 +1,17 @@
-from gambiarra.models import Chamado
+from gambiarra.models import Chamado, Acessorio
 from rest_framework import serializers
 
 class ChamadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chamado
         fields = ['titulo', 'descricao', 'professor', 'item', 'cliente']
+
+class AcessorioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Acessorio
+        filds = ['nome', 'item']
+
+
+
+
+  
