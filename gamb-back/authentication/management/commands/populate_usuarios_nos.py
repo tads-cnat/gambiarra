ca4372b609d16974d6d1b1dc09b6eeffd9193aad
Igor <santos.igor1@escolar.ifrn.edu.br>
2025-05-06 11:10:25 -0300

    🔧 chore: melhora a lógica da população de usuários e adiciona Gracon, Lua, Lili e Pitoco

--- gamb-back/authentication/management/commands/populate_usuarios_nos.py
+++ gamb-back/authentication/management/commands/populate_usuarios_nos.py
@@ -6,73 +6,84 @@ from django.conf import settings
 
 from authentication.models import Usuario
 
+
 class Command(BaseCommand):
     help = "Populate the User table with new users (Gerentes and Professores), including profile pictures"
 
+
     def handle(self, *args, **kwargs):
+        pasta = os.path.join(settings.BASE_DIR, 'authentication', 'management', 'commands', 'img')
+        def add_foto(usuario, nome):
+            imagem_nome = f"{nome}.png".lower()
+            imagem_caminho = os.path.join(pasta, imagem_nome)
+
+            if os.path.exists(imagem_caminho):
+                with open(imagem_caminho, 'rb') as img_file:
+                    usuario.imagem.save(imagem_nome, File(img_file), save=True) 
         try:
             grupo_admin = Group.objects.get(pk=1)
             grupo_professor = Group.objects.get(pk=2)
+            grupo_bolsista = Group.objects.get(pk=3)
+            grupo_servidor = Group.objects.get(pk=4)
+            grupo_cliente = Group.objects.get(pk=5)
+            grupo_aluno = Group.objects.get(pk=6)
 
-            pasta = os.path.join(settings.BASE_DIR, 'authentication', 'management', 'commands', 'img')
+            usuarios = [
+                #Gerentes
+                {"username": "igor", "grupo": grupo_admin},
+                {"username": "ryan", "grupo": grupo_admin},
+                {"username": "leo", "grupo": grupo_admin},
+                {"username": "lg", "grupo": grupo_admin},
+                {"username": "livia", "grupo": grupo_admin},
+            
+                #Professores
+                {"username": "jorgiano", "grupo": grupo_professor},
+                {"username": "lucena", "grupo": grupo_professor},
+                {"username": "braulio", "grupo": grupo_professor},
+                {"username": "marilia", "grupo": grupo_professor},
+                {"username": "felipao", "grupo": grupo_professor},
+                {"username": "demostenes", "grupo": grupo_professor},
+                {"username": "gracom", "grupo": grupo_professor},
 
-            def add_foto(usuario, nome):
-                imagem_nome = f"{nome}.png".lower()
-                imagem_caminho = os.path.join(pasta, imagem_nome)
+                #Bolsistas
+                {"username": "lili", "grupo": grupo_bolsista},
+                {"username": "lua", "grupo": grupo_bolsista},
+                {"username": "pitoco", "grupo": grupo_bolsista},
 
-                if os.path.exists(imagem_caminho):
-                    with open(imagem_caminho, 'rb') as img_file:
-                        usuario.imagem.save(imagem_nome, File(img_file), save=True)
+                #Servidores
 
-            gerentes = [
-                {"username": "igor", "email": "igor@admin.com", "grupo": grupo_admin},
-                {"username": "ryan", "email": "ryan@admin.com", "grupo": grupo_admin},
-                {"username": "leo", "email": "leo@admin.com", "grupo": grupo_admin},
-                {"username": "lg", "email": "lg@admin.com", "grupo": grupo_admin},
-                {"username": "livia", "email": "livia@admin.com", "grupo": grupo_admin},
+                #Clientes
+
+                #Alunos
+            
+            
             ]
 
-            for gerente in gerentes:
-                gerente_obj, created = Usuario.objects.update_or_create(
-                    username=gerente["username"],
-                    defaults={
-                        "email": gerente["email"],
-                        "is_staff": True,
+            for usuario in usuarios:
+                is_staff, is_superuser = False, False
+
+                #Permissões baseadas em populate_user.py, mas to achando estranho
+                if usuario["grupo"] in [grupo_admin, grupo_professor, grupo_bolsista]:
+                    is_staff = True
+                    is_superuser = True
+
+
+                usuario_obj, created = Usuario.objects.update_or_create(
+                    username = usuario["username"],
+                    defaults = {
+                        "email": "email@exemplo.com",
+                        "is_staff": is_staff,
+                        "is_superuser": is_superuser,
                         "is_active": True,
-                        "is_superuser": True,
-                        "grupo_id": gerente["grupo"].id,
+                        "grupo_id": usuario["grupo"].id,
                     },
                 )
-                if created:
-                    gerente_obj.set_password("ZAP123!!")
-                    add_foto(gerente_obj, gerente["username"])
-                    gerente_obj.save()
 
-            # Professores
-            professores = [
-                {"username": "jorgiano", "email": "jorgiano@admin.com", "grupo": grupo_professor},
-                {"username": "lucena", "email": "lucena@admin.com", "grupo": grupo_professor},
-                {"username": "braulio", "email": "braulio@admin.com", "grupo": grupo_professor},
-                {"username": "marilia", "email": "marilia@admin.com", "grupo": grupo_professor},
-                {"username": "felipao", "email": "felipao@admin.com", "grupo": grupo_professor},
-                {"username": "demostenes", "email": "demostenes@admin.com", "grupo": grupo_professor},
-            ]
-
-            for professor in professores:
-                professor_obj, created = Usuario.objects.update_or_create(
-                    username=professor["username"],
-                    defaults={
-                        "email": professor["email"],
-                        "is_staff": True,
-                        "is_active": True,
-                        "is_superuser": False,
-                        "grupo_id": professor["grupo"].id,
-                    },
-                )
                 if created:
-                    professor_obj.set_password("ZAP123!!")
-                    add_foto(professor_obj, professor["username"])
-                    professor_obj.save()
+                    usuario_obj.set_password("ZAP123!!")
+                    add_foto(usuario_obj, usuario["username"])
+                    usuario_obj.save()
+
 
         except Group.DoesNotExist:
             self.stderr.write(self.style.ERROR("Os grupos não existem"))
