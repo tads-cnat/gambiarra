98ff92c60719587920d576e27ec6010a4d1f58f0
liviaaVS <liviaeusla@gmail.com>
2025-05-27 20:00:27 -0300

    fix: corrige a divisão da variável ALLOWED_HOSTS para suportar múltiplos hosts

--- gamb-back/mysite/settings.py
+++ gamb-back/mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split()
 
 print(f"Allowed hosts: {ALLOWED_HOSTS}")
 
