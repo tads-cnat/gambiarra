d64b14700d0649b362d4a681ce5dc66ca534d822
liviaaVS <liviaeusla@gmail.com>
2025-05-27 19:45:24 -0300

    fix: adiciona IP ao ALLOWED_HOSTS para permitir acesso externo. #298

--- gamb-back/mysite/settings.py
+++ gamb-back/mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["pdsweb.pythonanywhere.com", "localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
