68cefbb41366495a2b33631eba12d2381e808373
LiviaaVS <liviaeusla@gmail.com>
2025-07-23 01:02:12 -0300

    :bug: fix: Atualiza o pipeline do Cypress

--- gamb-front/cypress/e2e/GambLogin.cy.ts
+++ gamb-front/cypress/e2e/GambLogin.cy.ts
@@ -29,5 +29,22 @@ describe("Teste com fixture", () => {
 		
 	});
 
-	
+	it("Deve realizar login com sucesso (interceptando resposta)", function () {
+		// Intercepta antes do clique!
+		cy.intercept("POST", "/api/v1/auth/login", {
+			statusCode: 200,
+			body: this.userData,
+		}).as("loginRequest");
+
+		cy.get("input[name='username']").type(this.userData.username);
+		cy.get("input[name='password']").type(this.userData.password);
+		cy.get("button[type='submit']").click();
+
+		cy.wait("@loginRequest");
+
+		// Verifica mensagem de sucesso se aplicável
+		cy.get("[data-cypress='success-messageLogin']")
+			.should("be.visible")
+			.and("contain", "Login realizado com sucesso!");
+	});
 });
