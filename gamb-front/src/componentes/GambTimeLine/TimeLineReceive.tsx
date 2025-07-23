6d5f21aae11ad2e3219ee3927dd6418de9c6f1e3
Luiz Gustavo OR <luizgustavo.orocha1@gmail.com>
2025-07-23 20:52:39 -0300

    :wrench: fix: Ajusta as cores da timeline para as certas

--- gamb-front/src/componentes/GambTimeLine/TimeLineReceive.tsx
+++ gamb-front/src/componentes/GambTimeLine/TimeLineReceive.tsx
@@ -2,6 +2,7 @@ import axiosInstance from "../../services/base/axiosInstance";
 import { useEffect, useState } from "react";
 import Timeline from "./TimeLine";
 import type { StatusType } from "../../interfaces/componentes/iGambTimeLine";
+import { statusConfig } from "./statusConfig";
 
 export function RequestTimeLine(props: { id: number }): React.JSX.Element {
 	const { id } = props;
@@ -16,28 +17,28 @@ export function RequestTimeLine(props: { id: number }): React.JSX.Element {
 					let label = "";
 					console.log("ALT", alt);
 					if (alt.status === "1") {
-						color = "#6c757d";
+						color = statusConfig["1"].color;
 						label = "Em análise";
 					} else if (alt.status === "2") {
-						color = "#28a745";
+						color = statusConfig["2"].color;
 						label = "Aceito";
 					} else if (alt.status === "3") {
-						color = "#FFC222";
+						color = statusConfig["3"].color;
 						label = "Em Diagnóstico";
 					} else if (alt.status === "4") {
-						color = "#28a745";
+						color = statusConfig["4"].color;
 						label = "Equipamento Em Conserto";
 					} else if (alt.status === "5") {
-						color = "#FFC222";
+						color = statusConfig["5"].color;
 						label = "Aguardando Peça";
 					} else if (alt.status === "6") {
-						color = "#28a745";
+						color = statusConfig["6"].color;
 						label = "Fechado Sem Resolução";
 					} else if (alt.status === "7") {
-						color = "#FFC222";
+						color = statusConfig["7"].color;
 						label = "Resolvido";
 					} else if (alt.status === "8") {
-						color = "#FFC222";
+						color = statusConfig["8"].color;
 						label = "Recusado";
 					}
 
