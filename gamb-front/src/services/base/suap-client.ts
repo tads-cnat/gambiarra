5340c11c6cf58325866adc4dd47ee5d142e50d10
LiviaaVS <liviaeusla@gmail.com>
2025-07-29 03:29:21 -0300

    fix: remove fallback value for CLIENT_ID and correct REDIRECT_URI variable name

--- gamb-front/src/services/base/suap-client.ts
+++ gamb-front/src/services/base/suap-client.ts
@@ -1,7 +1,7 @@
 import { SuapClient } from "./client";
 
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID_SUAP || "1ZhTRkW4vyAPp64qdfnqfhGWu5ZcpqWxRbiKpXki"
-const REDIRECT_URI =  import.meta.env.VITE_SUAP_REDIRECT_URI
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID_SUAP
+const REDIRECT_URI =  import.meta.env.VITE_REDIRECT_URI
 const SUAP_URL = "https://suap.ifrn.edu.br";
 const SCOPE = "identificacao email documentos_pessoais";
 
