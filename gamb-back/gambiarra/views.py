5b60cc869ba0cf25ff016aa560f684f462dda394
LiviaaVS <liviaeusla@gmail.com>
2025-07-29 22:57:16 -0300

    ajustes black

--- gamb-back/gambiarra/views.py
+++ gamb-back/gambiarra/views.py
@@ -108,7 +108,11 @@ class ChamadoViewSet(viewsets.ModelViewSet):
             queryset = Chamado.objects.all()
         elif grupo == GrupoEnum.PROFESSOR:
             queryset = Chamado.objects.filter(
-                Q(professor=user) | Q(status="1") | Q(status="6") | Q(status="9") | Q(status="8")
+                Q(professor=user)
+                | Q(status="1")
+                | Q(status="6")
+                | Q(status="9")
+                | Q(status="8")
             )
         elif grupo == GrupoEnum.BOLSISTA:
             queryset = Chamado.objects.filter(bolsistas=user)
